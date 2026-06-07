f189d55c9484d9d0746063f65a8e3cd63e982faf
Daniel Mendoza <danielmendozatorres500@gmail.com>
2026-06-07 15:49:11 -0700

    added print a and b value print to rotary encoder

--- Software/Bringup/rotary_encoder.py
+++ Software/Bringup/rotary_encoder.py
@@ -50,6 +50,7 @@ class QuadratureEncoder:
         """Handle changes on either encoder pin using quadrature logic."""
         current_a = self.pin_a.value()
         current_b = self.pin_b.value()
+        print(f"a= {current_a} b= {current_b}")
         
         # Only process on A pin changes for cleaner detent detection
         if current_a != self.last_a:
